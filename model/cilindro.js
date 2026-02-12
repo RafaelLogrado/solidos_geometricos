20aee1a3c9216eaee9b00123826758bfaf5a941e
RAFAEL QUEIROZ LOGRADO <rafael_logrado@estudante.sesisenai.org.br>
2026-02-12 08:59:56 -0300

    adicionado a classe cilindro

---
+++ model/cilindro.js
@@ -0,0 +1,20 @@
+class Cilindro{
+    constructor(raio,altura){
+        this.raio = raio
+        this.altura = altura
+    }
+
+    calcularArea(){
+        let area = 0.0
+        area = Math.PI * this.raio * this.raio
+        return area
+    }
+
+    calcularVolume(){
+        let volume = 0.0
+        volume = this.calcularArea() * this.altura
+        return volume
+    }
+}
+
+module.exports = Cilindro
